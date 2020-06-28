f05bd0c49afc53ee18813806600d970768ee0d3e
Gxx ECS <1152862327@qq.com>
2020-06-28 21:28:56 +0800

    update wechat load by scan

--- ordering_platform/src/main/java/com/gxx/ordering_platform/AppConfig.java
+++ ordering_platform/src/main/java/com/gxx/ordering_platform/AppConfig.java
@@ -130,7 +130,7 @@ public class AppConfig {
 			@Override
 			public void addResourceHandlers(ResourceHandlerRegistry registry) {
 				registry.addResourceHandler("/static/**").addResourceLocations("/static/");
-				registry.addResourceHandler("/j60UNhsMqa.txt").addResourceLocations("/j60UNhsMqa.txt");
+				registry.addResourceHandler("/wechat/**").addResourceLocations("/");
 			}
 		};
 	}
