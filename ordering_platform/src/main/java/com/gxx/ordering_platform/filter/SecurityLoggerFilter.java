411bab0b2ea7a70ed8a8fa559c8daa7c4b57e629
Gxx ECS <1152862327@qq.com>
2020-07-25 23:26:02 +0800

    finished securityLoggerFilter

--- ordering_platform/src/main/java/com/gxx/ordering_platform/filter/SecurityLoggerFilter.java
+++ ordering_platform/src/main/java/com/gxx/ordering_platform/filter/SecurityLoggerFilter.java
@@ -7,6 +7,7 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,7 +22,26 @@ public class SecurityLoggerFilter implements Filter {
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		// TODO Auto-generated method stub
+		HttpServletRequest req = (HttpServletRequest) request;
+		String ip = req.getHeader("x-forwarded-for");
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = req.getHeader("Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = req.getHeader("WL-Proxy-Client-IP");
+		}
+		if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+			ip = req.getRemoteAddr();
+		}
+		if (ip.indexOf(",") != -1) {
+			String[] ips = ip.split(",");
+			ip = ips[0].trim();
+		}
+		String url = req.getRequestURL().toString();
+		logger.info("start new visit---------------------");
+		logger.info("ip: " + ip);
+		logger.info("url: " + url);
+		logger.info(" ");
 		
 		chain.doFilter(request, response);
 	}
