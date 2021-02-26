3eabc6546c48859c92e5a4dedc0cb9ae94d485c8
Gxx ECS <1152862327@qq.com>
2021-02-27 02:05:52 +0800

    决定将价格字段修改为int，影响很大，需要修改很多！

--- ordering_platform/src/main/java/com/gxx/ordering_platform/controller/OSMController.java
+++ ordering_platform/src/main/java/com/gxx/ordering_platform/controller/OSMController.java
@@ -853,4 +853,23 @@ public class OSMController {
 		newJsonObject.put("meta", metaJsonObject);
 		return newJsonObject.toString();
 	}
+	
+//	@PostMapping(value = "/searchRefundPMonth", produces="application/json;charset=UTF-8")
+//	@ResponseBody
+//	public String searchRefundPMonth(@RequestBody Map<String, Object> map) {
+//		try {
+//			return oSMOrderingService.searchRefundPMonth(map);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		// 错误信息
+//		JSONObject newJsonObject = new JSONObject();
+//		
+//		JSONObject metaJsonObject = new JSONObject();
+//		metaJsonObject.put("status", 500);
+//		metaJsonObject.put("msg", "服务器错误，请联系管理员!");
+//		
+//		newJsonObject.put("meta", metaJsonObject);
+//		return newJsonObject.toString();
+//	}
 }
